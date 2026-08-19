0aac5a161e78b6e48d3fab798a22d8fd78469a82
saranyaaids2025-gif <saranya.aids2025@citchennai.net>
2026-08-19 13:08:15 +0530

    Add files via upload

---
+++ daa 10(output).py
@@ -0,0 +1,10 @@
+Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+================ RESTART: C:\Users\saranya\Desktop\DAA\daa 10.py ===============
+Input Type            DQS Comps   DQS Time(ms)      RQS Comps   RQS Time(ms)
+---------------------------------------------------------------------------
+Random                    70018           8.73          67889          10.46
+Sorted                 12497500        1504.10          74303          10.51
+Reverse                12497500        1163.90          76702          10.63
+Nearly Sorted            223313          25.71          72191           9.53
